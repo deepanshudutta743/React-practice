87de9cf9542c79ee8e8ea61207202a177fd13839
Deepanshu Dutta <deepanshudutta321@gmail.com>
2024-07-28 10:27:12 +0530

    new push

--- 04bgproject/src/App.jsx
+++ 04bgproject/src/App.jsx
@@ -11,7 +11,30 @@ function App() {
       <div className="w-full h-screen duration-200"
       style={{backgroundColor:color}}
       >
+     <div className='fixed flex flex-wrap
+     justify-center bottom-12 inset-x-0 px-2
+     '>
+      <div className='flex- flex-wrap justify-center
+      gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl'>
+      <button className='outline-none px-4 py-1
+      rounded-full text-white shadow-lg'
+      style={{backgroundColor:"red"}}
+      onClick={()=>setColor('red')}
+      >Red</button>
+            <button className='outline-none px-4 py-1
+      rounded-full text-white shadow-lg'
+      style={{backgroundColor:"Green"}}
+      onClick={()=>setColor('green')}
+      >Green</button>
+            <button className='outline-none px-4 py-1
+      rounded-full text-white shadow-lg'
+      style={{backgroundColor:"blue"}}
+      onClick={()=>setColor('blue')}
+      >blue</button>
 
+      </div>
+      
+     </div>
       </div>
     </>
   )
